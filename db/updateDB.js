4d560732fada5ae20835cd295f9e718bcd59af2e
heardman <markyheard@gmail.com>
2020-07-08 22:13:14 -0700

    making sure up to date

--- db/updateDB.js
+++ db/updateDB.js
@@ -2,33 +2,30 @@ var db = require("../models");
 var shipStationAPI = require("../apis/shipstationAPI");
 var updateDB = {
 
-  dateDiff: function (initial, final) {
+  datesBetween: function (initial, final) {
     //create a recursive function that assesses the date and adds a 
     var dates = [];
 
     function checkDates(init, fin) {
-      // console.log("INIT:"+init);
-      // console.log("FIN:"+fin);
-
-      //init.setTime(00);
 
       init.setHours(00);
       init.setMinutes(00);
       init.setSeconds(00);
 
-      //fin.setTime(00);
-
       fin.setHours(00);
       fin.setMinutes(00);
       fin.setSeconds(00);
 
-      if (init.toISOString().split("T")[0] === fin.toISOString().split("T")[0]) {
+      var newInitDay = init.getDate() + 1;
+      var newInitDate = new Date(initial.setDate(newInitDay));
+
+      if (newInitDate.toISOString().split("T")[0] === fin.toISOString().split("T")[0]) {
+
         console.log("dates are the same!")
 
       } else {
-        var newInitDay = init.getDate() + 1;
-        var newInitDate = new Date(initial.setDate(newInitDay));
-        console.log("NEW INIT DATE: "+newInitDate);
+
+        console.log("NEW INIT DATE: " + newInitDate);
         console.log("added");
         dates.push(newInitDate);
         checkDates(newInitDate, fin);
@@ -81,46 +78,56 @@ var updateDB = {
         console.log(err);
       }
 
-      //console.log(doc);
-
       // var today = new Date().toISOString().split("T")[0];
       var today = new Date();
       var docDate = new Date(doc.shipDate);
 
-      var pageSize = 100;
-
-      var filter = {
-        'shipDateStart': `${today}%2000:00:00`,
-        'includeShipmentItems': true,
-        'pageSize': pageSize
-      }
+      var anotherDay = new Date("07-01-2020");
 
       console.log("DOC DATE: " + docDate);
 
       console.log("TODAY DATE: " + today);
 
-      var dates = updateDB.dateDiff(docDate, today);
+      var dates = updateDB.datesBetween(docDate, today);
 
       console.log(dates);
 
+      if (dates.length > 0) {
 
-      // shipStationAPI.get('shipments', filter, function (data) {
-      //   updateDB.transposeShipments(data);
+        var pageSize = 100;
 
-      //   if (data.pages > 1) {
+        var filter = {
+          'shipDateStart': `${dates[0].toISOString().split("T")[0]}%2000:00:00`,
+          'shipDateEnd': `${dates[dates.length - 1].toISOString().split("T")[0]}%2000:00:00`,
+          'includeShipmentItems': true,
+          'pageSize': pageSize
+        }
 
-      //     for (var i = 2; i < data.pages + 1; i++) {
+        console.log(filter);
 
-      //       filter.page = i;
-      //       //console.log(filter);
-      //       shipStationAPI.get('shipments', filter, function (data) {
-      //         setTimeout(updateDB.transposeShipments, 2000, data);
-      //       });
+        shipStationAPI.get('shipments', filter, function (data) {
+          updateDB.transposeShipments(data);
 
-      //     }
-      //   }
+          if (data.pages > 1) {
 
-      // });
+            for (var i = 2; i < data.pages + 1; i++) {
+
+              filter.page = i;
+              //console.log(filter);
+              shipStationAPI.get('shipments', filter, function (data) {
+                setTimeout(updateDB.transposeShipments, 2000, data);
+              });
+
+            }
+          }
+
+        });
+      }
+
+      console.log("nothing to update!")
+
+      //end of shipstation update
+
     })
 
   },
